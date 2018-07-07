8570e2d99b3545b423d2b749a726e9b8c0b5a002
gknasln <gknasln@hotmail.com>
2018-07-07 21:20:54 +0300

    textarea update

--- javaNotes/131892/add/main.js
+++ javaNotes/131892/add/main.js
@@ -1,7 +1,62 @@
 var contentField = document.getElementById('content-field');
 
+
+document.getElementById('red-button').onclick = function(e){
+  e.preventDefault();
+  setSelection('red');
+}
+
+document.getElementById('blue-button').onclick = function(e){
+  e.preventDefault();
+  setSelection('blue');
+}
+
+document.getElementById('green-button').onclick = function(e){
+  e.preventDefault();
+  setSelection('green');
+}
+
+document.getElementById('bold-button').onclick = function(e){
+  e.preventDefault();
+  setSelection('b');
+}
+
+document.getElementById('underline-button').onclick = function(e){
+  e.preventDefault();
+  setSelection('u');
+}
+
+document.getElementById('overline-button').onclick = function(e){
+  e.preventDefault();
+  setSelection('strike');
+}
+
+
 contentField.addEventListener('keydown', function(e){
   if(e.altKey && e.key == 'Enter'){
-    contentField.value += "<hr/>";
+    // setSelection("code");
+    console.log(contentField.value.includes('\r'));
+  }
+  if(e.ctrlKey && e.key == 'b'){
+    setSelection("b");
+  }
+
+  if(e.ctrlKey && e.key == 'i'){
+    setSelection("i");
   } 
+
 });
+
+
+
+function setSelection(tagName){
+  var start = contentField.selectionStart;
+  var end = contentField.selectionEnd;
+  var selectedLength = end - start;
+  if(selectedLength == 0) return; 
+  var firstPart = contentField.value.substr(0, start);
+  var rest = contentField.value.substr(start);
+  var newText = firstPart + "<" + tagName + ">" + 
+      rest.substr(0, selectedLength) + "</" + tagName  + ">" + rest.substr(selectedLength);
+  contentField.value = newText;
+}
